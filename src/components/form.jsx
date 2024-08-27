41aa8477bfb6b19b1748da4fd9b5c50cbb234111
Miki <tamnhu03@gmail.com>
2024-08-27 15:14:26 +0700

    update (2)
    - create a form
    - create an input
    - display tasks in a list

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,30 +1,46 @@
 import { useState } from "react";
 
 function Form() {
-    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    function handleChange(e) {
+        setInputValue(e.target.value);
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert("The submit button is clicked!")
-    }
+        if (inputValue) {
+            setTasks([...tasks, inputValue]);
+            setInputValue('');
+        } else {
+            alert('Enter a task!')
+        }
+        
+    };
 
     return (
-        <div className="flex justify-center items-center min-h-screen">
-            <form className="flex gap-4 mb-12" onSubmit={handleSubmit}>
+        <div className="flex flex-col justify-center items-center min-h-screen p-4">
+            <form className="flex gap-4 mb-8" onSubmit={handleSubmit}>
                 <input
                     type="text"
-                    value={task}
+                    value={inputValue}
                     placeholder="Write your next task..."
-                    onChange={(e) => setTask(e.target.value)}
-                    className="w-80 h-12 p-4 border border-gray-300 rounded-md"
+                    onChange={handleChange}
+                    className="w-80 h-12 p-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500"
                 />
                 <button
                     type="submit"
-                    className="w-12 h-12 bg-green-700 text-white flex rounded-md items-center justify-center text-lg font-bold hover:bg-green-500"
+                    className="w-12 h-12 bg-green-700 text-white flex rounded-md items-center justify-center text-lg font-bold hover:bg-green-500 shadow-md"
                 >
-                    Add
+                    +
                 </button>
             </form>
+                <ul className="py-2 rounded-lg shadow-sm w-96 space-y-4">
+                        {tasks.map((tasks) => (
+                    <li key={tasks} className="py-2 pl-3 border border-white text-white rounded-md">{tasks}</li>
+                    ))}
+                </ul>
         </div>
     );
 }
